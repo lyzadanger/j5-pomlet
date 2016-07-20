f6910baca2f8bb315fa943c299e56b0a1dd2d663
Lyza Danger Gardner <lyza@lyza.com>
2016-07-20 09:36:11 -0400

    Add config module

--- main.js
+++ main.js
@@ -1,20 +1,26 @@
 'use strict';
+const config = require('./config');
 const five = require('johnny-five');
-const Pombit = require('./controllers/LCDButtons');
 
-const board = new five.Board();
+// Default options represent an Arduino with buttons, LED and LCD
+const options = Object.assign({}, {
+  board: new five.Board(),
+  Controller: require('./controllers/LCDButtons'),
+  pins: {
+    goBtn: 3,
+    otherBtn: 2,
+    downBtn: 4,
+    upBtn: 6,
+    led: 5,
+    metaBtn: 13,
+    lcd: [7, 8, 9, 10, 11, 12]
+  }
+}, config);
+const board = config.board || new five.Board();
 
 board.on('ready', () => {
-  const pombit = new Pombit({
-    pins: {
-      goBtn: 3,
-      otherBtn: 2,
-      downBtn: 4,
-      upBtn: 6,
-      led: 5,
-      metaBtn: 13,
-      lcd: [7, 8, 9, 10, 11, 12]
-    }
+  const pombit = new options.Controller({
+    pins: options.pins
   });
   pombit.go();
 });
